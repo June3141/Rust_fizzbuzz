5148f64abe2f848c2eb649e7a058fc307c8def32
june3141 <ryoushi0620@gmail.com>
2020-05-07 16:44:49 +0900

    任意の2組の素数で fizzbuzz 判定

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,26 @@ fn main() {
         (n, m)
     };
 
+    println!("Please input two primes.");
+
+    let s = {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        s.trim_end().to_owned()
+    };
+
+    let (prime_1, prime_2) = {
+        let mut ws = s.split_whitespace();
+        let n: u64 = ws.next().unwrap().parse().unwrap();
+        let m: u64 = ws.next().unwrap().parse().unwrap();
+        (n, m)
+    };
 
     for i in start..(end + 1) {
         let s;
         println!(
             "{}",
-            match (i % 3, i % 5, primal::is_prime(i)) {
+            match (i % prime_1, i % prime_2, primal::is_prime(i)) {
                 (0, 0, _) => "FizzBuzz",
                 (0, _, _) => "Fizz",
                 (_, 0, _) => "Buzz",
