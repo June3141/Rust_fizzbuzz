57441e87f73aeb50fbea2eef5398c637ed5aefa9
june3141 <ryoushi0620@gmail.com>
2020-05-07 16:52:22 +0900

    読み取り部分を関数化した．

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,25 @@ extern crate primal;
 
 use std::io;
 
+fn read_nums_pair() -> (u64, u64) {
+    let s = {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        s.trim_end().to_owned()
+    };
+
+    let mut ws = s.split_whitespace();
+    let n: u64 = ws.next().unwrap().parse().unwrap();
+    let m: u64 = ws.next().unwrap().parse().unwrap();
+    (n, m)
+}
+
 fn main() {
     println!("Please input start and end.");
-
-    let s = {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-        s.trim_end().to_owned()
-    };
-
-    let (start, end) = {
-        let mut ws = s.split_whitespace();
-        let n: u64 = ws.next().unwrap().parse().unwrap();
-        let m: u64 = ws.next().unwrap().parse().unwrap();
-        (n, m)
-    };
+    let (start, end) = read_nums_pair();
 
     println!("Please input two primes.");
-
-    let s = {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-        s.trim_end().to_owned()
-    };
-
-    let (prime_1, prime_2) = {
-        let mut ws = s.split_whitespace();
-        let n: u64 = ws.next().unwrap().parse().unwrap();
-        let m: u64 = ws.next().unwrap().parse().unwrap();
-        (n, m)
-    };
+    let (prime_1, prime_2) = read_nums_pair();
 
     for i in start..(end + 1) {
         let s;
